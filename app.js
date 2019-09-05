dbc75a8f1e6bd87a9f923c74ce5ee65e873dc243
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2019-09-05 12:08:10 +0300

    Add bug as view template engin

--- app.js
+++ app.js
@@ -14,8 +14,12 @@ app.use("/css", express.static(path.join(__dirname, "/node_modules/bootstrap/dis
 app.use("/js", express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js")));
 app.use("/js", express.static(path.join(__dirname, "/node_modules/jquery/dist")));
 
+app.set("views", "./src/views");
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+  // res.sendFile(path.join(__dirname, "views", "index.html"));
+  res.render("index", { list: ["a", "b"] });
 });
 
 app.listen(port, () => {
